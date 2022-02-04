17c97c8b80c38549ed015de6ad430e20adb9a923
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-04 15:02:38 +0100

    books.js created

---
+++ data/books.js
@@ -0,0 +1,17 @@
+export const books = [
+	{
+		title: "Permanent Record",
+		description: "I listened to Edward Snowden talk to Joe Rogan and was impressed by how down to earth Edward Snowden is, and how smart he is. Impressive. So I had to read his autobiography.",
+		price: 12.95
+	},
+	{
+		title: "The Book of Why",
+		description: "Got this book from my daughter for Christmas 2020. It seems to be a good analysis of what the difference is, exactly, between correlation and causation, which has very much to do with daily decisions we make, both minor and major.",
+		price: 18.95
+	},
+	{
+		title: "Waking Up",
+		description: `I have listened to a few of Sam Harris' talks and find him to be a someone who has quite a bit to say about spirituality and belief primarily from a neurological perspective. This is his latest book with promising chapter titles such as "The Mystery of Consciousness" and "The Riddle of the Self".`,
+		price: 21.50
+	}
+];
