7abe0f7e4cca3a6c24373387a485db2820eb9b5e
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-02-04 15:24:50 +0100

    displaying book data

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ import { books } from '../data/books.js';
 
 const contentElem = document.querySelector('.content');
 
-contentElem.innerHTML = `<h2>${books.length} Books</h2>`;
+contentElem.innerHTML += `<h2>${books.length} Books</h2>`;
+
+contentElem.innerHTML += books.map(book => {
+	return `
+	<div className="book">
+		<div className="title">${book.title}</div>
+		<div className="description">${book.description}</div>
+		<div className="price">${book.price}</div>
+	</div>
+`;
+}).join('');
+
